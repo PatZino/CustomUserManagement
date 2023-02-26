0a0f0dceac3ece3548e64ef8d668d56f16138e6d
PatZino <eedawarez@gmail.com>
2023-02-26 20:04:23 +0100

    Add project files.

---
+++ Enums/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomUserManagement.Enums
+{
+    public enum Roles
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
---
+++ Models/ManageUserRolesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomUserManagement.Models
+{
+    public class ManageUserRolesViewModel
+    {
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+        public bool Selected { get; set; }
+    }
+}
